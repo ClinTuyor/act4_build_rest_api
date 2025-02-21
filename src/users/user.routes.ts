abea61e4e8974273195a33d084f0a6bc3bf1e986
{Your_Name} <Clint Tuyor>
2025-02-21 20:22:37 +0800

    user routes done

--- src/users/user.routes.ts
+++ src/users/user.routes.ts
@@ -10,7 +10,7 @@ userRouter.get("/users", async (req: Request, res: Response) => {
         const allUsers: UnitUser[] = await database.findAll();
 
         if (allUsers.length === 0) {
-            return res.status(StatusCodes.NOT_FOUND).json({ msg: 'No users at this time...' });
+            return res.status(StatusCodes.NOT_FOUND).json({ msg: `No users at this time...` });
         }
 
         return res.status(StatusCodes.OK).json({ total_users: allUsers.length, users: allUsers });
@@ -24,11 +24,99 @@ userRouter.get("/users/:id", async (req: Request, res: Response, next: Function)
         const user : UnitUser = await database.findOne(req.params.id);
 
         if (!user) {
-            return res.status(StatusCodes.NOT_FOUND).json({ error: 'User not found!' });
+            return res.status(StatusCodes.NOT_FOUND).json({ error: `User not found!` });
         }
 
-        return res.status(StatusCodes.OK).json({ user });
+        return res.status(StatusCodes.OK).json({user});
     } catch (error) {
         return next (error);
     } 
 })
+
+userRouter.post("/register", async (req: Request, res: Response) => {
+    try {
+        const { username, email, password} = req.body;
+
+        if (!username || !email || !password) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ error: `Please provide all the required parameters...` });
+        }
+
+        const user = await database.findbyEmail(email);
+
+        if (user) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ error: `This email has already been registered...` });
+        }
+
+        const newUser = await database.create(req.body);
+
+        return res.status(StatusCodes.CREATED).json({ newUser });
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error });
+    }
+})
+
+userRouter.post("/login", async (req: Request, res: Response) => {
+    try {
+        const {email, password} = req.body;
+
+        if (!email || !password) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ error: "Please provide all the required parameters..." });
+        }
+
+        const user = await database.findbyEmail(email);
+
+        if (!user) {
+            return res.status(StatusCodes.NOT_FOUND).json({ error: "No user exists with the email provided..." });
+        }
+
+        const comparePassword = await database.comparePassword(email, password);
+
+        if (!comparePassword) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ error: `Incorrect password...` });
+        }
+
+        return res.status(StatusCodes.OK).json({user});
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error });
+    }
+})
+
+userRouter.put('/user/:id', async (req: Request, res: Response) => {
+    try {
+        const {username, email, password} = req.body;
+
+        const getUser = await database.findOne(req.params.id);
+
+        if (!username || !email || !password) {
+            return res.status(401).json({ error: `Please provide all the required parameters...` });
+        }
+        
+        if (!getUser) {
+            return res.status(404).json({ error: `No user with id ${req.params.id}`});
+        }
+
+        const updateUser = await database.update((req.params.is), req.body);
+
+        return res.status(200).json({ updateUser });
+    }catch (error) {
+        return res.status(500).json({ error });
+    }
+})
+
+userRouter.delete("/user/:id", async (req: Request, res: Response) => {
+    try {
+        const id = (req.params.id);
+
+        const user = await database.findOne(id);
+
+        if (!user) {
+            return res.status(StatusCodes.NOT_FOUND).json({ error: `User does not exist` });
+        }
+
+        await database.remove(id);
+
+        return res.status(StatusCodes.OK).json({ msg: `User deleted` });
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error });
+    }
+});
