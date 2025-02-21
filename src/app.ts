bfa8fc50182b699b76f335e79f57bc8fdc09dba3
{Your_Name} <Clint Tuyor>
2025-02-21 20:31:49 +0800

    app.ts update

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
+import { userRouter } from "./users/user.routes";
 
 dotenv.config();
 
 if (!process.env.PORT) {
-    console.error('No PORT value specified...');
+    console.error(`No PORT value specified...`);
     process.exit(1);
 }
 
@@ -18,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
+
+app.use('/', userRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
